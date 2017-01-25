7cb10643de7ec714b0024d7a9c902dc7fc764a94
Anand <anand@hioscar.com>
2017-01-25 09:01:03 -0500

    Updated main

---
+++ sputr.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+#####################################
+# generate_payloads.py
+# created: 2017-01-20
+# author: seth
+#####################################
+
+def main():
+	usage()
+
+def usage():
+	print "Usage instructions <here>"
+
+
+if __name__ == "__main__":
+	main()
