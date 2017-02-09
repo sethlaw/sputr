0a5da4693384773c8d29915738006ace6028b6b9
Anand <anand@hioscar.com>
2017-02-09 15:14:33 -0500

    updates

--- testgen/basetest.py
+++ testgen/basetest.py
@@ -1,7 +1,16 @@
+#base class for all unit test generators
 class Test():
 	def __init__(self):
 		print("init")
 	
 	def get_payloads(self):
 		print("payloads")
-		#make a call to "generate_payloads.py" and get the appropriate payloa
+		#make a call to "generate_payloads.py" and get the appropriate payloads
+	def setUp(self):
+		#do setup
+
+	def tearDown(self):
+		#do tear down
+	
+	def mockService(self):
+		#mock a service call
