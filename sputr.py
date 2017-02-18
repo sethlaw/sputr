b04e92ce4eb0a43138ee3b2fce273b6ef11f383d
Seth Law <seth@nvisium.com>
2017-02-17 20:49:53 -0700

    changes to main sputr.py

--- sputr.py
+++ sputr.py
@@ -17,18 +17,26 @@ def main():
 	parser = argparse.ArgumentParser(description='sputr.py')
 	parser.add_argument('--config',dest='config',default='config.json',help='config file (default: config.json)')
 	parser.add_argument('--test',action='store_true', help='start tests')
+	parser.add_argument('--generate',action='store_true', help='generate config file for app')
+	parser.add_argument('--testcsrf',action='store_true', help='test csrf from initial dev')
 	
 	args = parser.parse_args()
 	
 	
-	payloads =	{
-		"username":"admin",
-		"password":"password",
-		"email":"example@email.com"
-		}
-	url = "http://localhost:1234/api/login"
-	poc = POCGenerationService(payloads)
-	poc.writeToFile(poc.csrf_poc(url))
+	if args.testcsrf: 
+		payloads =	{
+			"username":"admin",
+			"password":"password",
+			"email":"example@email.com"
+			}
+		url = "http://localhost:1234/api/login"
+		poc = POCGenerationService(payloads)
+		poc.writeToFile(poc.csrf_poc(url))
+	elif args.test:
+		c = parse_config(args.config)
+	else:
+		parser.print_help()
+	return 0
 
 def generate_config():
 	#TEST
@@ -41,8 +49,8 @@ def generate_config():
 
 
 
-def parse_config():
-	with open('config.json','r') as config:
+def parse_config(f):
+	with open(f,'r') as config:
 		d = json.loads(config.read())
 	return d
 
