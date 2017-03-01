69243602586e62575b512271fc9aad1732372387
Seth Law <seth@nvisium.com>
2017-02-28 20:08:27 -0700

    initial pass at config generator

--- sputr.py
+++ sputr.py
@@ -21,6 +21,9 @@ def main():
 	parser.add_argument('--config',dest='config',default='config.json',help='config file (default: config.json)')
 	parser.add_argument('--test',action='store_true', help='start tests')
 	parser.add_argument('--generate',action='store_true', help='generate config file for app')
+	parser.add_argument('--apptype',dest='apptype',help='application type for config generation (django|flask|spring|dotnet)')
+	parser.add_argument('--appdir',dest='appdir',help='application directory for config generation')
+	parser.add_argument('--output',dest='output',default='config.json',help='file to output config file to')
 	parser.add_argument('--testcsrf',action='store_true', help='test csrf from initial dev')
 	
 	args = parser.parse_args()
@@ -38,28 +41,62 @@ def main():
 		poc.writeToFile(poc.csrf_poc(url))
 	elif args.test:
 		c = parse_config(args.config)
+		pp.pprint(c)
 		creds = c['creds']
 		domain = c['domain']
-		#pp.pprint(c)
 		for ep in c['endpoints']:
 			tests = list(ep['tests'])
+			if tests[0] == '1':
+				print('running sqli tests')
 			if tests[1] == '1':
+				print('running xss tests')
 				xss_payloads = Payloads.generate_payloads('xss')
 				xss = xss_test(ep,domain,creds,xss_payloads,DEBUG=False)
 				xss.test()
-				
+			if tests[2] == '1':
+				print('running IDOR tests')
+			if tests[3] == '1':
+				print('running CSRF tests')
+			if tests[4] == '1':
+				print('running access control tests')
+	elif args.generate:
+		print('generating config from ' + args.appdir + ' as a ' + args.apptype + ' application to ' + args.output)
+		generate_config(args.appdir,args.apptype,args.output)
 	else:
 		parser.print_help()
 	return 0
 
-def generate_config():
+def generate_config(appdir,apptype,output):
 	#TEST
 	config = {}
-	config['authkey'] = 'bananas'
-	config['/v1/api/dashboard'] = 111100
-	config_json = json.dumps(config)
-	with open('config.json','w') as f:
-		json.dump(config_json,f)
+	config['token'] = {}
+	config['token']['name']= 'cookie_name'
+	config['token']['value']= 'cookie_value'
+	config['creds'] = {}
+	config['creds']['username'] = {}
+	config['creds']['username']['name']= 'username'
+	config['creds']['username']['value']= 'testuser'
+	config['creds']['password'] = {}
+	config['creds']['password']['name']= 'password'
+	config['creds']['password']['password']= 'temppass'
+	config['csrf'] = {}
+	config['csrf']['pattern'] = '^regexpattern$'
+	config['csrf']['name'] = 'csrftokenname'
+	config['domain'] = {}
+	config['domain']['host'] = 'localhost:8000'
+	config['domain']['protocol'] = 'http://'
+	config['domain']['login_url'] = 'http://localhost:8000/taskManager/login'
+	config['domain']['auth_url'] = 'http://localhost:8000/taskManager/dashboard'
+	config['endpoints'] = []
+	ep = {}
+	ep['path'] = '/'
+	ep['method'] = 'GET'
+	ep['auth'] = 0
+	ep['params'] = {}
+	ep['tests'] = "11111"
+	config['endpoints'].append(ep)
+	with open(output,'w') as f:
+		json.dump(config,f,indent=4)
 
 
 
